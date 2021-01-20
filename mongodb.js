4b57464de64cb4dba55bd87c138e31b00258face
Devin Sather <devin.sather@outlook.com>
2021-01-20 12:38:14 -0800

    Added test suite

--- mongodb.js
+++ mongodb.js
@@ -2,59 +2,59 @@
 // const MongoClient = mongodb.MongoClient
 // const ObjectID = mongodb.ObjectID
 
-const  { MongoClient, ObjectID } = require('mongodb')
+// const  { MongoClient, ObjectID } = require('mongodb')
 
-const connectionURL = 'mongodb://127.0.0.1:27017'
-const databaseName = 'task-manager'
+// const connectionURL = 'mongodb://127.0.0.1:27017'
+// const databaseName = 'task-manager'
 
-MongoClient.connect(connectionURL, { useNewUrlParser: true }, (error, client) => {
-  for (let i = 0; i < 10; i++) {
-    console.log('------------------------------------------------')
-  }
-  console.log('\n\n')
-  if (error) {
-    return console.log('Unable to connect to database!')
-  }
+// MongoClient.connect(connectionURL, { useNewUrlParser: true }, (error, client) => {
+//   for (let i = 0; i < 10; i++) {
+//     console.log('------------------------------------------------')
+//   }
+//   console.log('\n\n')
+//   if (error) {
+//     return console.log('Unable to connect to database!')
+//   }
   
-  const db = client.db(databaseName)
+//   const db = client.db(databaseName)
 
-  // db.collection('users').updateOne( {
-  //   _id: new ObjectID("5fffac4fcfe86f097c07c21c")
-  // }, {
-  //   $inc: {
-  //     age: 1
-  //   }
-  // }).then((result) => {
-  //   console.log(result)
-  // }).catch((error) => {
-  //   console.log(error)
-  // })
+//   db.collection('users').updateOne( {
+//     _id: new ObjectID("5fffac4fcfe86f097c07c21c")
+//   }, {
+//     $inc: {
+//       age: 1
+//     }
+//   }).then((result) => {
+//     console.log(result)
+//   }).catch((error) => {
+//     console.log(error)
+//   })
 
-  // db.collection('tasks').updateMany({
-  //   completed: false
-  // }, {
-  //   $set: {
-  //     completed: true
-  //   }
-  // }).then((result) => {
-  //   console.log(result.modifiedCount)
-  // }).catch((error) => {
-  //   console.log(error)
-  // })
+//   db.collection('tasks').updateMany({
+//     completed: false
+//   }, {
+//     $set: {
+//       completed: true
+//     }
+//   }).then((result) => {
+//     console.log(result.modifiedCount)
+//   }).catch((error) => {
+//     console.log(error)
+//   })
 
-  // db.collection('users').deleteMany({
-  //   age: 28
-  // }).then((result) => {
-  //   console.log(result)
-  // }).catch((error) => {
-  //   console.log(error)
-  // })
+//   db.collection('users').deleteMany({
+//     age: 28
+//   }).then((result) => {
+//     console.log(result)
+//   }).catch((error) => {
+//     console.log(error)
+//   })
 
-  // db.collection('tasks').deleteOne({
-  //   description: 'Empty Trash Bin'
-  // }).then((result) => {
-  //   console.log(result)
-  // }).catch((error) => {
-  //   console.log(error)
-  // })
-})
+//   db.collection('tasks').deleteOne({
+//     description: 'Empty Trash Bin'
+//   }).then((result) => {
+//     console.log(result)
+//   }).catch((error) => {
+//     console.log(error)
+//   })
+// })
